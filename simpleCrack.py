1992d2639d332fb3a63296429648688fe4f1b0a7
Smpoile90 <simon.m.poile@gmail.com>
2018-08-13 13:36:43 +0100

    Working

--- simpleCrack.py
+++ simpleCrack.py
@@ -1,6 +1,6 @@
 import re
 import argparse
-import hashlib
+import crypt
 
 parser = argparse.ArgumentParser(prog="Unshadower")
 parser.add_argument('-w', '--wordlist', help = "Word list to use", required = True)
@@ -12,7 +12,7 @@ users = []
 
 def main():
     #get the users with a shadowed password
-    with open(args['shadowfile']) as file:
+    with open(args['shadowfile'],"r",encoding='utf-8') as file:
         for line in file:
             if regex.search(line):
                 users.append(line)
@@ -20,42 +20,36 @@ def main():
     for user in users:
         profiles.append(profile(user))
 
-    print(profiles)
+    for p in profiles:
+        print(p)
+        p.crack()
         
-
 class profile():
     def __init__(self, ul):
         fields = ul.split('$')
         self.name = fields[0] 
         self.algo = fields[1]
         self.salt = fields[2]
-        self.uhash = (fields[3].split(':'))[0]
+        self.uhash = (ul.split(':'))[1].encode('utf-8')
+
+    def __str__(self):
+        string = self.name + ' ' + self.algo + ' ' + self.salt
+        return string
 
     def crack(self):
-        if(self.algo == '1'):
-            m = hashlib.md5
-        elif self.algo == '2a' | '2y':
-            ##
-            pass            
-        elif self.algo == '5'
-            m=hashlib.sha256
-        elif self.algo == '6'
-            m=hashlib.sha512
-        else:
-            print("Hash not recognised")
-            return
-
         ##Hash all of the words in the wordlist and compare
-        with read(args['wordlist']) as wl:
-            for w in wl:
-            x = hashlib.sha256()
-            x.update(w)
-            x.update(self.salt)    
-            h = x.hexdigest()
-            if (h == self.uhash)
-                result = name + ':' + w +'\n' + "Password Found!"
-                break
-            result = "Password Not Found" 
+        with open(args['wordlist'],"r",encoding='utf-8') as wl:
+            for w in wl:     
+                testHash = crypt.crypt(w.rstrip(),salt='$'+self.algo+'$'+self.salt)
+                testHash=testHash.encode('utf-8')
+
+                if testHash == self.uhash:
+                    result = self.name + ':' + w +'\n' + "Password Found!"
+                    break
+                else:
+                    result = "Password Not Found" 
+
+        print (result)
 
 if __name__ == "__main__":
     main()
